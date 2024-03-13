211d95076ad44f764a8018cd9e5e0b1894916100
joelomar15 <omarjoeltene@gmail.com>
2024-03-13 14:40:40 -0500

    ssa

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,6 +1,6 @@
 <?php
 	$conexion = new mysqli("localhost","root","","proyeccionesgfs"); 
 	if (mysqli_connect_errno()) {
-		echo "No se puede conectar 🚫";
+		echo "No se puede conectar 🚫"; 
 	}
 ?>
